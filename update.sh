a1afaace104bf634ac7e452688f3510cfb09a892
Vitalie Spinu <spinuvit@gmail.com>
2018-09-19 21:50:19 +0200

    Add update.sh for automatic upstream fetches

--- update.sh
+++ update.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Example:
+# update.sh 1.22
 
 version=$1
 
@@ -6,10 +8,10 @@ git submodule update --remote
 cd inst/include
 git checkout $version
 cd -
-printf "### $(date +%F)\n-update to v$version upstream\n\n" | cat - NEWS.md > NEWS.tmp
+printf "### $(date +%F)\n- update to upstream v$version\n\n" | cat - NEWS.md > NEWS.tmp
 mv NEWS.tmp NEWS.md
 sed -i "s/Version:.*$/Version: $version/g" DESCRIPTION
 sed -i "s/Date:.*$/Date: $(date +%F)/g" DESCRIPTION
 git add . 
-git commit -m "Update upstream v$version"
+git commit -m "Update to upstream v$version"
 git tag v$version 
